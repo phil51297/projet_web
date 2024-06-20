aecd0189e4bfd11d1c093f83220e182d3f47f258
ulysseGUILLOT <ulysseguillot@gmail.com>
2024-06-20 09:54:05 +0200

    correct sendMessage return

--- back/src/message/message.resolver.ts
+++ back/src/message/message.resolver.ts
@@ -3,25 +3,25 @@ import { MessageService } from './message.service';
 import { Message } from './models/message.model';
 import { UserService } from '../user/user.service';
 
-@Resolver(of => Message)
+@Resolver((of) => Message)
 export class MessageResolver {
   constructor(
     private messageService: MessageService,
     private userService: UserService,
   ) {}
 
-  @Query(returns => [Message])
+  @Query((returns) => [Message])
   messagesByConversation(@Args('conversationId') conversationId: string) {
     return this.messageService.findMessagesByConversation(conversationId);
   }
 
-  @Mutation(returns => Message)
+  @Mutation((returns) => Message)
   async sendMessage(
     @Args('conversationId') conversationId: number,
     @Args('userId') userId: string,
     @Args('text') text: string,
-  ): Promise<Message> {
+  ) {
     const user = this.userService.findOneById(userId);
-    return this.messageService.sendMessage(conversationId, user, text);
+    return await this.messageService.sendMessage(conversationId, user, text);
   }
 }
