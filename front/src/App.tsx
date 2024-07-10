17cfcbf372e4196c9aa2d3326a3aeedeb440b20a
eliotmeurillon <eliot.meurillon@ooeo.fr>
2024-07-10 15:47:20 +0200

    chore: Update user avatar and name display in the navigation menu

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -691,17 +691,19 @@ function App() {
                   href="#"
                   className="flex items-center p-2 text-base font-medium text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
                 >
-                  <svg
-                    aria-hidden="true"
-                    className="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path>
-                    <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
-                  </svg>
-                  <span className="ml-3">Overview</span>
+                  <div className="flex items-center gap-4">
+                    <img
+                      className="w-10 h-10 rounded-full"
+                      src="https://avatar.iran.liara.run/public/boy"
+                      alt=""
+                    />
+                    <div className="font-medium dark:text-white">
+                      <div>Karim 93150</div>
+                      <div className="text-sm text-gray-500 dark:text-gray-400">
+                        t ou sal fils de pute rend mon quade
+                      </div>
+                    </div>
+                  </div>
                 </a>
               </li>
             </ul>
