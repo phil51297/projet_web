b260c36ec9b9ec4ba85bec5a47a79325dfe28cb9
eliotmeurillon <eliot.meurillon@ooeo.fr>
2024-07-11 11:47:16 +0200

    chore: Add @clerk/clerk-react dependency and integrate with Apollo client in main.tsx

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -2,9 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { ClerkProvider } from "@clerk/clerk-react";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
+
+const client = new ApolloClient({
+  uri: "http://localhost:3000/graphql",
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+        <App />
+      </ClerkProvider>
+    </ApolloProvider>
   </React.StrictMode>
 );
