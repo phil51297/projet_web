ce262191159346e93065ba184c1f4d00fc997504
eliotmeurillon <eliot.meurillon@ooeo.fr>
2024-07-11 14:48:47 +0200

    chore: Update Clerk publishable key

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -5,7 +5,8 @@ import "./index.css";
 import { ClerkProvider } from "@clerk/clerk-react";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+const PUBLISHABLE_KEY =
+  "pk_test_ZGlzY3JldGUtc2VhZ3VsbC04OS5jbGVyay5hY2NvdW50cy5kZXYk";
 
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
