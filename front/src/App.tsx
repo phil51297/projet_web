65f61e605f7dbab4e23dbd85193cdadfd89def70
eliotmeurillon <eliot.meurillon@ooeo.fr>
2024-07-11 10:49:13 +0200

    chore: Add Lucide-React library for SVG icons and update dependencies

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,3 +1,5 @@
+import { SendHorizonal } from "lucide-react";
+
 function App() {
   return (
     <>
@@ -780,15 +782,7 @@ function App() {
                   type="submit"
                   className="inline-flex justify-center p-2 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100 dark:text-blue-500 dark:hover:bg-gray-600"
                 >
-                  <svg
-                    className="w-5 h-5 rotate-90 rtl:-rotate-90"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor"
-                    viewBox="0 0 18 20"
-                  >
-                    <path d="m17.914 18.594-8-18a1 1 0 0 0-1.828 0l-8 18a1 1 0 0 0 1.157 1.376L8 18.281V9a1 1 0 0 1 2 0v9.281l6.758 1.689a1 1 0 0 0 1.156-1.376Z" />
-                  </svg>
+                  <SendHorizonal />
                   <span className="sr-only">Send message</span>
                 </button>
               </div>
